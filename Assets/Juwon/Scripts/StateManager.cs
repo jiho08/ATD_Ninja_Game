9f1bcf3fff2a59ef88cd6bab5132e8d172219840
pormer <juwonst@gmail.com>
2024-04-10 21:20:34 +0900

    StateManager 기초 작성

--- Assets/Juwon/Scripts/StateManager.cs
+++ Assets/Juwon/Scripts/StateManager.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class StateManager : MonoBehaviour
 {
-    
+    private Mugunghwa _mugung;
+
+
 }
 
 public class Mugunghwa
 {
-    public float health = 10f;
-    public float speed = 5f;
+    public float health = 10f; //체력
+    public float speed = 10f; //속도
+    public float atk = 20f; //공격력
+    public float coolTime = 10f; //쿨타임
 }
