29c316342be3f76e6e3a010dc01edf29f5909b6f
pormer <juwonst@gmail.com>
2024-04-17 20:19:22 +0900

    싱글톤 수정

--- Assets/Juwon/Scripts/SpawnManager.cs
+++ Assets/Juwon/Scripts/SpawnManager.cs
@@ -8,27 +8,27 @@ public class SpawnManager : MonoBehaviour
     [SerializeField] PoolManager enemyPool;
 
     [SerializeField] private Transform[] spawnPos; //스폰 위치 3개 관리
-    
+
     private int[] defaultSpawnCounts = new int[10]; //유닛 수만큼 넣기 (0 : 무궁화, 1 : 무언가, 2 : 등등)
     private int[] getSpawnCounts = new int[10]; //소환된 유닛 수만큼 넣기 (0 : 무궁화, 1 : 무언가, 2 : 등등)
 
 
-    [SerializeField] GameObject UnitSpawn(int value)
+    [SerializeField] GameObject UnitSpawn(int value, Transform pos)
     {
         if(getSpawnCounts[value] > defaultSpawnCounts[value]) return null; //정해진 수보다 많아지면 리턴
         
         GameObject unit = unitPool.Get(value);
-        unit.transform.position = spawnPos[0].position; //정해진 소환할 위치에 소환(우선 정함)
+        unit.transform.position = pos.position; //정해진 소환할 위치에 소환(우선 정함)
         unit.GetComponent<HealthManager>().SetHp(StateManager.Instance.GetUnitHP(value)); //HP설정
 
         getSpawnCounts[0]++;
         return unit;
     }
 
-    [SerializeField] GameObject EnemySpawn(int value, Transform setPos)
+    [SerializeField] GameObject EnemySpawn(int value, Transform pos)
     {
         GameObject enemy = enemyPool.Get(value);
-        enemy.transform.position = setPos.position;
+        enemy.transform.position = pos.position;
         return enemy;
     }
 
