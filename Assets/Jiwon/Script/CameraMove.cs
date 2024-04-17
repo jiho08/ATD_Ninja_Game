6ba9c0ee64f93541e5f83919252309b8536faf0b
jiwon0612 <iololloli9976@gmail.com>
2024-04-18 00:54:02 +0900

    인성선배가 도와주신 카메라 무빙 프로토타입

--- Assets/Jiwon/Script/CameraMove.cs
+++ Assets/Jiwon/Script/CameraMove.cs
@@ -7,27 +7,29 @@ public class CameraMove : MonoBehaviour
     private Vector3 mousePositiion;
     private Vector3 wordPosition;
 
+    [SerializeField] private float _minX;
+    [SerializeField] private float _maxX;
+
     private Camera camera;
     private void Start()
     {
         camera = Camera.main;
-        
+
 
     }
 
     private void Update()
     {
-        
-
         if (Input.GetKey(KeyCode.C))
         {
-            ScrenentoWorld();
-            transform.position = wordPosition;
+            // x축 방향으로 1만큼 이동 후 제한 적용
+            float newX = Mathf.Clamp(transform.position.x + 1, _minX, _maxX);
+
+            // 새로운 x값을 이용해 카메라의 위치 업데이트
+            transform.position = new Vector3(newX, transform.position.y, transform.position.z);
         }
-        else
-        {
-            
-        }
+
+
     }
 
     private void ScrenentoWorld()
@@ -37,5 +39,5 @@ public class CameraMove : MonoBehaviour
         wordPosition = Camera.main.ScreenToWorldPoint(mousePositiion + new Vector3(0, 0, 10));
     }
 
-    
+
 }
