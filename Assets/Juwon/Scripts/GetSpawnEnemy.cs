edbad1bad9f48d93f23e6b2f84cf51ae875b85e5
pormer <juwonst@gmail.com>
2024-04-20 14:04:51 +0900

    스테이지 별로 원하는데로 적 생성 완성

---
+++ Assets/Juwon/Scripts/GetSpawnEnemy.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class GetSpawnEnemy : MonoBehaviour
+{
+    [SerializeField] TextAsset[] stageRes;
+    private List<EnemySpawn> spawnsL;
+
+    private void Awake() 
+    {
+        spawnsL = new List<EnemySpawn>();
+        
+    }
+
+    //point, type, delay에 맞게 텍스트 읽기
+    public void ReadSpawnFile(int value)
+    {
+        spawnsL.Clear();
+
+        TextAsset textFile = stageRes[value];
+
+        StringReader stringReader = new StringReader(textFile.text);
+
+        while (stringReader != null) {
+            string line = stringReader.ReadLine();
+
+            if(line == null) {
+                break;
+            }
+
+            EnemySpawn enemySpawnData = new()
+            {
+                delay = float.Parse(line.Split('/')[0]),
+                type = line.Split('/')[1],
+                point = int.Parse(line.Split('/')[2])
+            };
+            spawnsL.Add(enemySpawnData);
+        }
+        stringReader.Close();
+    }
+
+    //리스트 보네기
+    public List<EnemySpawn> GetSpawnsList()
+    {
+        return spawnsL;
+    }
+
+}
+
+public class EnemySpawn
+{
+    public float delay;
+    public string type;
+    public int point;
+}
