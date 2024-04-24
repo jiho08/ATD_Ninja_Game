e4a608e65e28a0ecbb1530de3e66a667c944bfd6
pormer <juwonst@gmail.com>
2024-04-24 19:55:06 +0900

    이상

--- Assets/Juwon/Scripts/SpawnManager.cs
+++ Assets/Juwon/Scripts/SpawnManager.cs
@@ -13,14 +13,13 @@ public class SpawnManager : MonoBehaviour
     private int[] defaultSpawnCounts = new int[10]; //유닛 수만큼 넣기 (0 : 무궁화, 1 : 무언가, 2 : 등등)
     private int[] getSpawnCounts = new int[10]; //소환된 유닛 수만큼 넣기 (0 : 무궁화, 1 : 무언가, 2 : 등등)
 
-
     //원하는 유닛과 위치 생성
     public GameObject UnitSpawn(int value, int pos)
     {
         if(getSpawnCounts[value] > defaultSpawnCounts[value]) return null; //정해진 수보다 많아지면 리턴
         
         GameObject unit = unitPool.Get(value);
-        unit.transform.position = unitSpawnPos[pos-1].position; //정해진 소환할 위치에 소환(우선 정함)
+        //unit.transform.position = unitSpawnPos[pos-1].position; //정해진 소환할 위치에 소환(우선 정함)
         unit.GetComponent<HealthManager>().Health = StateManager.Instance.GetUnitHP(value); //HP설정
 
         getSpawnCounts[value]++;
