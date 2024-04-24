80890ce52467e92888bfa9ddec3cbae4d6bdd59d
pormer <juwonst@gmail.com>
2024-04-24 18:12:13 +0900

    Health 수정

--- Assets/Juwon/Scripts/HealthBar.cs
+++ Assets/Juwon/Scripts/HealthBar.cs
@@ -10,14 +10,14 @@ public class HealthBar : MonoBehaviour
     [SerializeField] private HealthManager health;
 
     private void Start() {
-        _maxHpValue = health.GetHp();
+        _maxHpValue = health.Health;
     }
 
     void LateUpdate()
     {
-        _curHpValue = health.GetHp();
+        _curHpValue = health.Health;
         //만약 맞았다면 if로 넣기
         hpBarObj.transform.localScale = new Vector3(_curHpValue / _maxHpValue * 5f, 0.5f, 0.5f); //healthManager에서 최대체력과 현재 체력 넣기
-        if(_curHpValue <= 0) health.SetHp(0);
+        if(_curHpValue <= 0) health.Health = 0;
     }
 }
