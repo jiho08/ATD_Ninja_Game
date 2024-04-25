c4f79a73a3e762a2085c9d5274bef667450a9003
Renga <argeiaArgia@gmail.com>
2024-04-25 20:28:40 +0900

    페이드 인아웃 240425

--- Assets/Gaheon/Scripts/OtherThings/BlackSceneFade.cs
+++ Assets/Gaheon/Scripts/OtherThings/BlackSceneFade.cs
@@ -1,55 +1,43 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
+
 
 public class BlackSceneFade : MonoBehaviour
 {
     Image blackPanel;
-    private void Start()
+
+    public float fadeSpeed = 1.5f;
+
+    void Start()
     {
         blackPanel = GetComponent<Image>();
-        Debug.Log("asdasd");
+        blackPanel.color = new Color(0f, 0f, 0f, 1f);
         StartCoroutine(FadeOut());
+    }
+
+
+
+    IEnumerator FadeIn()
+    {
+        while (blackPanel.color.a < 1)
+        {
+            blackPanel.color += new Color(0, 0, 0, Time.deltaTime * fadeSpeed);
+            yield return null;
+        }
+        gameObject.SetActive(true);
     }
 
     IEnumerator FadeOut()
     {
-        blackPanel.color -= new Color(0f, 0f, 0f, 0.125f);
-        yield return new WaitForSecondsRealtime(0.125f);
-        blackPanel.color -= new Color(0f, 0f, 0f, 0.125f);
-        yield return new WaitForSecondsRealtime(0.125f);
-        blackPanel.color -= new Color(0f, 0f, 0f, 0.125f);
-        yield return new WaitForSecondsRealtime(0.125f);
-        blackPanel.color -= new Color(0f, 0f, 0f, 0.125f);
-        yield return new WaitForSecondsRealtime(0.125f);
-        blackPanel.color -= new Color(0f, 0f, 0f, 0.125f);
-        yield return new WaitForSecondsRealtime(0.125f);
-        blackPanel.color -= new Color(0f, 0f, 0f, 0.125f);
-        yield return new WaitForSecondsRealtime(0.125f);
-        blackPanel.color -= new Color(0f, 0f, 0f, 0.125f);
-        yield return new WaitForSecondsRealtime(0.125f);
-        blackPanel.color -= new Color(0f, 0f, 0f, 0.125f);
-        gameObject.SetActive(false);
-    }
-    IEnumerator FadeIn()
-    {
-        blackPanel.color += new Color(0f, 0f, 0f, 0.125f);
-        yield return new WaitForSecondsRealtime(0.125f);
-        blackPanel.color += new Color(0f, 0f, 0f, 0.125f);
-        yield return new WaitForSecondsRealtime(0.125f);
-        blackPanel.color += new Color(0f, 0f, 0f, 0.125f);
-        yield return new WaitForSecondsRealtime(0.125f);
-        blackPanel.color += new Color(0f, 0f, 0f, 0.125f);
-        yield return new WaitForSecondsRealtime(0.125f);
-        blackPanel.color += new Color(0f, 0f, 0f, 0.125f);
-        yield return new WaitForSecondsRealtime(0.125f);
-        blackPanel.color += new Color(0f, 0f, 0f, 0.125f);
-        yield return new WaitForSecondsRealtime(0.125f);
-        blackPanel.color += new Color(0f, 0f, 0f, 0.125f);
-        yield return new WaitForSecondsRealtime(0.125f);
-        blackPanel.color += new Color(0f, 0f, 0f, 0.125f);
+        while (blackPanel.color.a > 0)
+        {
+            blackPanel.color -= new Color(0, 0, 0, Time.deltaTime * fadeSpeed);
+            yield return null;
+        }
         gameObject.SetActive(false);
     }
 }
