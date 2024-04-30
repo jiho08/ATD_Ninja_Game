aead7f68aa4db027ba7dc1441cad8db03a4456c8
jiwon0612 <iololloli9976@gmail.com>
2024-04-30 10:14:35 +0900

    소환범위 레일 전체로 변경 어디서 소환하든 역에서 소환

--- Assets/Jiwon/Script/RailInput.cs
+++ Assets/Jiwon/Script/RailInput.cs
@@ -6,14 +6,26 @@ public class RailInput : MonoBehaviour
 {
     public static bool onRail;
 
+    private BoxCollider2D collider;
+    private Bounds bounds;
     public static Vector2 raillTrans;
 
+
+    private void Awake()
+    {
+        collider = GetComponent<BoxCollider2D>();
+    }
+    private void Start()
+    {
+        bounds = collider.bounds;
+        raillTrans = new Vector2(bounds.min.x, bounds.center.y);
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             onRail = true;
-            raillTrans = new Vector2(transform.position.x,transform.position.y);
+            raillTrans = new Vector2(bounds.min.x, bounds.center.y);
 
         }
   
