c4a3dff344064b2d2bc60e31c85c112017b27044
pormer <juwonst@gmail.com>
2024-05-01 19:43:11 +0900

    체력 변수 수정

--- Assets/Juwon/Scripts/States/HealthManager.cs
+++ Assets/Juwon/Scripts/States/HealthManager.cs
@@ -3,11 +3,25 @@ using UnityEngine;
 public class HealthManager : MonoBehaviour
 {
     //Hp가 들어가는 유닛에 넣기
-    [SerializeField] private float curHealth;
+    [SerializeField] private float curHealth = 1;
 
-    public float Health { get; set; }
+    public float Health
+    {
+        get => curHealth;
+        set
+        {
+            curHealth = value;
+            
+            if (curHealth <= 0)
+            {
+                gameObject.SetActive(false);
+            }
+            
+        }
+    }
 
-    public float Damage { get; set; } = 0;
+    public float Damage { get; set; }
 
     //자신의 HP가 0이라면 비활성화
+    
 }
