76e19f89d243d36bf7a5ba88e6ec52b7cc53c4e5
jiho08 <57872222k@gmail.com>
2024-05-03 01:29:43 +0900

    타이틀 찐찐찐최종완성본.에셋유효
    진짜로

--- Assets/Jiho/Scripts/SubwayMovement.cs
+++ Assets/Jiho/Scripts/SubwayMovement.cs
@@ -7,6 +7,7 @@ public class SubwayMovement : MonoBehaviour
     [SerializeField] float _speed = 10f;
     [SerializeField] float _scrollRange = 50f;
     [SerializeField] float _angle = -45f;
+    [SerializeField] float _anglePoint = 45f;
     [SerializeField] Transform _target;
     Rigidbody2D _rigid;
 
@@ -23,12 +24,12 @@ public class SubwayMovement : MonoBehaviour
             transform.position = _target.position + Vector3.left * _scrollRange;
         }
 
-        if (transform.position.x >= 115)
+        if (transform.position.x >= _anglePoint)
         {
             transform.rotation = Quaternion.AngleAxis(_angle, Vector3.forward);
-            _rigid.velocity = new Vector2(_speed, -30);
+            _rigid.velocity = new Vector2(_speed, -8);
         }
-        if (transform.position.x < 115)
+        if (transform.position.x < _anglePoint)
         {
             transform.rotation = Quaternion.AngleAxis(0, Vector3.forward);
             _rigid.velocity = new Vector2(_speed, 0);
