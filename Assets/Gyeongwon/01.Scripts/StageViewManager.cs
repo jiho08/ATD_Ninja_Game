4f3e19d04614d8fde4f912812aa7f0346dfd674c
ruddnjs13 <ruddnjs013@gmail.com>
2024-05-04 11:25:24 +0900

    커밋

---
+++ Assets/Gyeongwon/01.Scripts/StageViewManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class StageViewManager : MonoBehaviour
+{
+    [SerializeField] public Image StageIamge;
+    [SerializeField] public TextMeshProUGUI StageName;
+
+    public void SetStageView(int value)
+    {
+        //StageName.text = 
+    }
+}
