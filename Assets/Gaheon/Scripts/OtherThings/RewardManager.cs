81d93d549571abe068af2244a1ea96f99a4d46cb
Renga <argeiaArgia@gmail.com>
2024-05-08 00:49:38 +0900

    보상으로 콜라를 드리겠습니다

---
+++ Assets/Gaheon/Scripts/OtherThings/RewardManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RewardManager : MonoBehaviour
+{
+    [SerializeField] Image stationImage;
+    [SerializeField] TextMeshProUGUI clearText;
+    [SerializeField] TextMeshProUGUI[] rewardTexts;
+
+    private void Start()
+    {
+        RewardChange();
+    }
+    void RewardChange()
+    {
+    }
+}
---
+++ Assets/Gaheon/Scripts/SO/StationImageSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "SO/OtherThing/StationImages")]
+public class StationImageSO : ScriptableObject
+{
+    public Image StationImages;
+}
