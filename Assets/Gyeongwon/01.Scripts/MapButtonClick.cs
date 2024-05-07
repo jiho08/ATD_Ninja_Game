629d1c61d14099fb16879ed262be606803da3571
ruddnjs13 <ruddnjs013@gmail.com>
2024-05-08 00:45:14 +0900

    월드맵 완성

--- Assets/Gyeongwon/01.Scripts/MapButtonClick.cs
+++ Assets/Gyeongwon/01.Scripts/MapButtonClick.cs
@@ -1,27 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class MapButtonClick : MonoBehaviour
 {
     public Camera mainCam;
     public GameObject MapPos;
     bool mapButtonClicked;
-    [SerializeField] GameObject Stages;
-    [SerializeField] GameObject StageViews;
-    [SerializeField] GameObject RedDot;
+   
 
 
     private void Update()
     {
         if (mapButtonClicked)
         {
-            mainCam.transform.position = Vector3.Slerp(mainCam.transform.position, MapPos.transform.position, 0.02f);
+            mainCam.transform.position = Vector3.Slerp(mainCam.transform.position, MapPos.transform.position, 0.01f);
         }
         else if (!mapButtonClicked)
         {
-            mainCam.transform.position = Vector3.Slerp(mainCam.transform.position, new Vector3(0,0,-10), 0.02f);
+            mainCam.transform.position = Vector3.Slerp(mainCam.transform.position, new Vector3(0,0,-10), 0.01f);
         }
 
 
