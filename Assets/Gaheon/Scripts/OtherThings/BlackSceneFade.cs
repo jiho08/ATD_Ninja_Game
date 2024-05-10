8f47669e49ec645af5a8b7acd3ebd5580f1256cc
jiwon0612 <iololloli9976@gmail.com>
2024-05-10 13:44:19 +0900

    치명적오류 수정

--- Assets/Gaheon/Scripts/OtherThings/BlackSceneFade.cs
+++ Assets/Gaheon/Scripts/OtherThings/BlackSceneFade.cs
@@ -16,39 +16,46 @@ public class BlackSceneFade : MonoBehaviour
 
     void Start()
     {
-        StartCoroutine(FadeOutCo());
+        Time.timeScale = 1;
+        FadeOutCo();
         _timeSet = Time.deltaTime;
     }
 
-    IEnumerator FadeInCo(int scneneNum)
+
+    void FadeInCo(int scneneNum)
     {
-        blackPanel.transform.localScale = Vector3.one;
+        blackPanel.color = new Color(0f, 0f, 0f, 0f);
+        //blackPanel.transform.localScale = Vector3.one;
+        Sequence seq1 = DOTween.Sequence();
+        seq1.SetUpdate(true);
+        seq1.Append(blackPanel.DOFade(1, 1f)).OnComplete(() => SceneManager.LoadScene(scneneNum));
 
-        while (blackPanel.color.a < 1)
-        {
-            blackPanel.color += new Color(0, 0, 0,  _timeSet * fadeSpeed);
-            yield return null;
-        }
+        //while (blackPanel.color.a <= 1)
+        //{
 
-        SceneManager.LoadScene(scneneNum);
+        //    blackPanel.color += new Color(0, 0, 0, _timeSet * fadeSpeed);
+        //}
+
+     
     }
 
-    IEnumerator FadeOutCo()
+    void FadeOutCo()
     {
-        Time.timeScale = 1;
-        blackPanel.color = new Color(0f, 0f, 0f, 1f);
-        while (blackPanel.color.a > 0)
-        {
-            blackPanel.color -= new Color(0, 0, 0, Time.deltaTime * fadeSpeed);
-            yield return null;
-        }
 
-        blackPanel.transform.localScale = Vector3.zero;
+        blackPanel.color = new Color(0f, 0f, 0f, 1f);
+        //while (blackPanel.color.a >= 0)
+        //{
+        //    blackPanel.color -= new Color(0, 0, 0, Time.deltaTime * fadeSpeed);
+        //    yield return null;
+        //}
+        blackPanel.DOFade(0, 1f);
+
+        //blackPanel.transform.localScale = Vector3.zero;
     }
 
     public void ExitScene(int scneneNum)
     {
-        StartCoroutine(FadeInCo(scneneNum));
+        FadeInCo(scneneNum);
         AudioManager.Instance.PlaySfx(AudioManager.Sfx.Btn);
     }
 }
